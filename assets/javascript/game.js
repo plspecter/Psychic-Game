1e2dccdd47f04066e4f11089b334a291cd929568
Perri Specter <plspecter@gmail.com>
2018-08-14 23:23:19 -0400

    Psychic-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var wins = 0
 var losses = 0
-var guessesleft = [9]
+var guessesleft = 10
 var guessessofar = []
 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -8,7 +8,7 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 document.onkeyup = function(event) {
 var userGuess = event.key;
 guessessofar.push(userGuess)
-guessesleft.push(guessesleft - 1)
+guessesleft = guessesleft-1;
 
 if (userGuess == computerGuess) {
     wins++; 
@@ -26,6 +26,9 @@ document.getElementById("game").innerHTML = html;
 
 //how to reset the guesses left & guesses so far
 
+for (var i = 0; i < computerChoices.length; i++) {
+
+}
 
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
